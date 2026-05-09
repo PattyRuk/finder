d5e59d84bde86220b9825ccd945093a76629f2ea
Manvir kaur <manvirkaur8193@gmail.com>
2026-05-08 21:26:44 -0500

    added jd-2

--- assets/script/index.js
+++ assets/script/index.js
@@ -18,4 +18,26 @@ loginBtn.addEventListener('click', function (event) {
     const storedUsername = localStorage.getItem('username');
     const storedEmail = localStorage.getItem('email');
     const storedPassword = localStorage.getItem('password');
+
+     errorMsg.textContent = '';
+
+   
+    if (!username && !password) {
+        errorMsg.textContent = 'Username and Password are required';
+        return;
+    }
+    if (!username) {
+        errorMsg.textContent = 'Username or Email is required';
+        return;
+    }
+    if (!password) {
+        errorMsg.textContent = 'Password is required';
+        return;
+    }
+
+    if ((username === storedUsername || username === storedEmail) && password === storedPassword) {
+        window.location.href = './user.html';
+    } else {
+        errorMsg.textContent = 'Incorrect username or password';
+    }
 });
