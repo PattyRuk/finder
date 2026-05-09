e49eb8c573857cf85d7355f6ed520b751fc2032b
Manvir kaur <manvirkaur8193@gmail.com>
2026-05-08 21:25:02 -0500

    added js-1

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const usernameInput = document.querySelector('#username');
+const passwordInput = document.querySelector('#password');
+const loginBtn = document.querySelector('.login-btn');
+const errorMsg = document.querySelector('.error-msg');
+
+localStorage.setItem('username', 'Jane123');
+localStorage.setItem('email', 'Jane.Doe@email.com');
+localStorage.setItem('password', 'password123');
+
+loginBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const username = usernameInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    const storedUsername = localStorage.getItem('username');
+    const storedEmail = localStorage.getItem('email');
+    const storedPassword = localStorage.getItem('password');
+});
